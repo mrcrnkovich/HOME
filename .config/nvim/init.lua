cb52cb1125ae614ba427b9ed3eeeae12bb1ee03f
Mike Crnkovich <mrcrnkovich@gmail.com>
2024-01-04 20:44:39 -0500

    Initial Nvim commit

---
+++ .config/nvim/init.lua
@@ -0,0 +1,76 @@
+require('mrcrnkovich.set')
+require('mrcrnkovich.remap')
+require('mrcrnkovich.packer')
+require('mrcrnkovich.lsp')
+
+-- Load snippets from ~/.config/nvim/LuaSnip/
+require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/lua/LuaSnip/"})
+require("luasnip").config.set_config({
+  enable_autosnippets = true,
+  store_selection_keys = "<Tab>",
+})
+
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+local Format = augroup('Format', {})
+local TestCompile = augroup('TestCompile', {})
+
+-- Need to include getcurpos and setpos to save / restore the
+-- cursor position upon save
+autocmd('BufWrite', {
+    group   = Format,
+    pattern = '*.go',
+    callback = function()
+        old_pos = vim.api.nvim_win_get_cursor(0)
+        vim.cmd([[:%!goimports]])
+        vim.api.nvim_win_set_cursor(0, old_pos)
+    end,
+})
+
+autocmd('BufWrite', {
+    group   = Format,
+    pattern = '*.{t,pm,pl}',
+    callback = function()
+        old_pos = vim.api.nvim_win_get_cursor(0)
+        vim.cmd([[:%!perltidy]])
+        vim.api.nvim_win_set_cursor(0, old_pos)
+    end,
+})
+
+autocmd('BufWrite', {
+    group   = Format,
+    pattern = '*.py',
+    callback = function()
+        old_pos = vim.api.nvim_win_get_cursor(0)
+        vim.cmd([[:%!black --quiet -]])
+        vim.api.nvim_win_set_cursor(0, old_pos)
+    end,
+})
+
+--default
+autocmd( 'FileType' , {
+    group = TestCompile,
+    pattern = '*',
+    callback = function()
+        vim.keymap.set('n', '<Leader>c', ':make<CR>')
+    end,
+})
+
+--specifc overrides
+autocmd( 'FileType' , {
+    group = TestCompile,
+    pattern = '*.go',
+    callback = function()
+        vim.keymap.set('n', '<Leader>c', ':GoTest<CR>')
+    end,
+})
+
+autocmd( 'FileType' , {
+    group = TestCompile,
+    pattern = 'perl',
+    callback = function()
+        vim.keymap.set('n', '<Leader>c', ':!perl -c %<CR>')
+    end,
+})
+
---
+++ .config/nvim/lua/mrcrnkovich/packer.lua
@@ -0,0 +1,32 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+-- vim.cmd.packadd('packer.nvim')
+
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+
+    -- Git
+    use('tpope/vim-fugitive')
+
+    -- Fuzzy Find Everything
+    use {'junegunn/fzf', cmd = 'fzf#install()' }
+    use('junegunn/fzf.vim')
+
+      -- Appearance Plugins
+    use('itchyny/lightline.vim')
+    use('morhetz/gruvbox')
+
+    -- Go specific
+    -- use('fatih/vim-go')
+
+	-- LSP Support
+    use('neovim/nvim-lspconfig')
+
+      -- Autocomplete
+    use('hrsh7th/nvim-cmp')
+    use('hrsh7th/cmp-nvim-lsp')
+    use('L3MON4D3/LuaSnip')
+
+end)
