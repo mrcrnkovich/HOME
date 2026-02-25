9bceca287fa6f2a19e3772f0d40b2d073e77ce9a
Mike Crnkovich <mike@mcrnkovich.io>
2026-02-24 21:10:49 -0500

    Setup Perldoc command
    Adds a perl specific command, similar to Man and Godoc
    to generate perl docs within nvim.

--- .config/nvim/lua/nvim-tools/docs.lua
+++ .config/nvim/lua/nvim-tools/docs.lua
@@ -25,11 +25,43 @@ local function godoc(cmd, package, ...)
   set_options()
 end
 
+local function perldoc(cmd, package, ...)
+    local function set_options()
+      vim.bo.swapfile = false
+      vim.bo.buftype = 'nofile'
+      vim.bo.bufhidden = 'unload'
+      vim.bo.modified = false
+      vim.bo.readonly = true
+      vim.bo.modifiable = false
+      vim.bo.filetype = 'man'
+    end
+
+  local page = vim.system({"perldoc", package,}, {text = true, timeout = 10000 }):wait()
+
+  vim.cmd([[:vertical new<CR>]])
+  vim.cmd.file({ 'perldoc://' .. package , mods = { silent = true } })
+
+  vim.bo.modifiable = true
+  vim.bo.readonly = false
+  vim.bo.swapfile = false
+
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.split(page.stdout, '\n'))
+  vim.cmd('1') -- Move cursor to first line
+  set_options()
+end
+
 function M.Setup()
   vim.api.nvim_create_user_command('Godoc',
     function(params)
       godoc({"go", "doc", "-all"}, params.args)
       vim.cmd[[highlight godocTitle guifg=darkred gui=bold]]
+    end,
+    { nargs = 1 }
+  )
+
+  vim.api.nvim_create_user_command('Perldoc',
+    function(params)
+      perldoc({"perldoc"}, params.args)
     end,
     { nargs = 1 }
   )
